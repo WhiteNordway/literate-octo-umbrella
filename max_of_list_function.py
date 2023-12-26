2efa16158026717daaec450b9b00c0cb45759630
WhiteNordway <75182209+WhiteNordway@users.noreply.github.com>
2023-12-26 04:28:27 +0300

    #5

--- max_of_list_function.py
+++ max_of_list_function.py
@@ -1,2 +1,8 @@
 def max_of_list(lst):
    return max(lst)
+if __name__ == "__main__":
+   quantity = int(input("Введите число элементов массива: "))
+   lst = input("Введите элементы массива через пробел: ").split()
+   if len(lst) > quantity:
+      lst = lst[:quantity]
+   print(f"{max_of_list(lst) = }")
